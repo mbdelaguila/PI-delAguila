46d528e2706e949fee8227678a0e42719d996b15
mbdelaguila <delaguila.mb@gmail.com>
2022-07-26 19:32:26 +0200

    frontend2

--- Frontend/pi/README.md
+++ Frontend/pi/README.md
@@ -1,4 +1,4 @@
-# PIDelAguila
+# Pi
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.0.
 
--- Frontend/pi/src/app/app.component.spec.ts
+++ Frontend/pi/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PI-delAguila'`, () => {
+  it(`should have as title 'pi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PI-delAguila');
+    expect(app.title).toEqual('pi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PI-delAguila app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pi app is running!');
   });
 });
