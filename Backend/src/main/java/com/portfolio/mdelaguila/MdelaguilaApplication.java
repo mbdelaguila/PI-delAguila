0b5d721daef19698acfc653bab8a6da9a0cf2051
mbdelaguila <delaguila.mb@gmail.com>
2022-07-27 12:18:52 +0200

    Backend

---
+++ Backend/src/main/java/com/portfolio/mdelaguila/MdelaguilaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mdelaguila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdelaguilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdelaguilaApplication.class, args);
+	}
+
+}
